5c610a8e49487c14839262fc2a696de37eab8863
lyy <33707637+liaoyongyu@users.noreply.github.com>
2018-09-12 20:56:50 +0800

    Update logistic_regression.py

--- classify/NeutralNetwork/logistic_regression.py
+++ classify/NeutralNetwork/logistic_regression.py
@@ -1,14 +1,12 @@
 # coding: utf-8
 
-
 import numpy as np 
 import matplotlib.pyplot as plt
 import h5py
 import scipy
 from PIL import Image
 from scipy import ndimage
-import numpy as np
-import h5py
+
        
 def load_dataset():
     train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
@@ -30,6 +28,7 @@ train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_datas
 m_train = train_set_x_orig.shape[0]   
 m_test = test_set_x_orig.shape[0]
 num_px = train_set_x_orig.shape[1]
+
 #flatten平坦化
 train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
 test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
@@ -51,11 +50,8 @@ def initialize_with_zeros(dim):
     assert(isinstance(b, float) or isinstance(b, int))
     return w, b
 
-
 # cost函数，计算梯度
 def propagate(w, b, X, Y):
-
-    
     m = X.shape[1]      
     A = sigmoid(np.dot(w.T, X) + b)            # compute activation   [1,m]
     cost = -1 / m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1 - A))         # compute cost
