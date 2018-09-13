42b22c2722c12faf3c321dcea572a8c880ddde25
lyy <33707637+liaoyongyu@users.noreply.github.com>
2018-09-13 09:26:33 +0800

    Update logistic_regression.py

--- classify/NeutralNetwork/logistic_regression.py
+++ classify/NeutralNetwork/logistic_regression.py
@@ -6,19 +6,15 @@ import h5py
 import scipy
 from PIL import Image
 from scipy import ndimage
-
        
 def load_dataset():
     train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
     train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
     train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
-
     test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
     test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
     test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
-
     classes = np.array(test_dataset["list_classes"][:]) # the list of classes
-    
     train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
     test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
     return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
@@ -68,31 +64,22 @@ def propagate(w, b, X, Y):
     
 # 梯度下降
 def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost):
-
-    costs = []
-    
+    costs = []    
     for i in range(num_iterations):
         grads, cost = propagate(w, b, X, Y)
         dw = grads["dw"]
         db = grads["db"]
         w = w - learning_rate * dw
         b = b - learning_rate * db
-        
         if i % 100 == 0:
             costs.append(cost)
         if print_cost and i % 100 == 0:
-            print ("Cost after iteration %i: %f" %(i, cost))
-		
+            print ("Cost after iteration %i: %f" %(i, cost))		
     params = {"w": w,
               "b": b}
-    
     grads = {"dw": dw,
              "db": db}
-    
     return params, grads, costs
-
-
-
 
 #传入优化的w，b   
 def predict(w, b, X):
@@ -109,22 +96,17 @@ def predict(w, b, X):
     
 	return Y_prediction
 
-
 def model(X_train, Y_train, X_test, Y_test, num_iterations, learning_rate, print_cost):
     # initialize parameters with zeros (≈ 1 line of code)
     w, b = initialize_with_zeros(X_train.shape[0]) # w[12288,1],b=0
-
     # Gradient descent (≈ 1 line of code)
     parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
-    
     # Retrieve parameters w and b from dictionary "parameters"
     w = parameters["w"]
     b = parameters["b"]
-    
     # Predict test/train set examples (≈ 2 lines of code)
     Y_prediction_test = predict(w, b, X_test)
     Y_prediction_train = predict(w, b, X_train)
-
     # Print train/test Errors rate
     print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
     print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))    
@@ -139,18 +121,16 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations, learning_rate, print
 #训练模型
 d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 1000, learning_rate = 0.003, print_cost = False)
 
-#不同学习的速率 
+#不同学习的速率的效果
 learning_rates = [0.003, 0.001, 0.0001]
 models = {}
 for i in learning_rates:
     print ("learning rate is: " + str(i))
     models[str(i)] = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 1500, learning_rate = i, print_cost = True)
     print ('\n' + "-------------------------------------------------------" + '\n')
-
 #提取cost和rate
 for i in learning_rates:
     plt.plot(np.squeeze(models[str(i)]["costs"]), label= str(models[str(i)]["learning_rate"]))
-
 plt.ylabel('cost')
 plt.xlabel('iterations')
 legend = plt.legend(loc='upper center', shadow=True)
